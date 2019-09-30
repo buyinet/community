ed12aa584a0bf00d27462294735ccb39589e3b36
buyinet <2453201633@qq.com>
2019-09-30 20:26:51 +0800

    'commit'

--- src/main/java/com/bywlkjs/community/controller/HelloController.java
+++ src/main/java/com/bywlkjs/community/controller/HelloController.java
@@ -8,10 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String greeting(@RequestParam(name="name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
-    }
+    @GetMapping("/")
+    public String greeting(){ return "index"; }
 
 }
