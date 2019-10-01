9fd07edc8c21f143f1a64f0c844fa5f47c56f7b7
buyinet <2453201633@qq.com>
2019-10-01 13:36:59 +0800

    'callback'

--- src/main/java/com/bywlkjs/community/controller/HelloController.java
+++ src/main/java/com/bywlkjs/community/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloController {
@@ -11,4 +12,9 @@ public class HelloController {
     @GetMapping("/")
     public String greeting(){ return "index"; }
 
+    @GetMapping("/callback")
+    public String callback(@RequestParam("code") String code){
+        System.out.println("callback下获取的code"+code);
+        return null;
+    }
 }
